a13019bec061171803b7ee1a875cd894f657183c
kyrlouca <kyrlouca@gmail.com>
2020-05-27 11:00:38 +0300

    interviews not working

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,23 +12,7 @@ module.exports = {
       twitter: `Kyrlouca`,
     },
   },
-  plugins: [
-    {
-      resolve: "gatsby-remark-embed-video",
-      options: {
-        width: 800,
-        ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-        height: 400, // Optional: Overrides optional.ratio
-        related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
-        noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
-        urlOverrides: [
-          {
-            id: 'youtube',
-            embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
-          }
-        ] 
-      }
-    },
+  plugins: [    
     
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-remark`,
